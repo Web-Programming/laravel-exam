490e8995e61dd9d03a3f8048bf6f0add00c71150
Nur Rachmat <rachmat.nur91@gmail.com>
2023-10-30 08:39:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Insturuksi
+1. Clone repository ini
+2. Jalankan perintah ```composer install```
+3. Jalankan perintah ```php artisan key:generate```
+4. Kerjakan sesuai petunjuk soal
+5. Lengkapi kode atau buka komentar pada file yang yang telah diberi tanda ```-- lihat soal```
+
+
+## Referensi
+Anda dapat mengakses dokumentasi Laravel https://laravel.com/docs/10.x untuk membantu anda mengerjakan soal-soal.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Ujian Semester Pemrograman Web - Laravel</title>
+</head>
+<body>
+
+
+<div class="container py-5">
+  <h1 class="h2">Selamat Menempuh Ujian Semester</h1>
+  <p>Ini adalah base project untuk ujian semester matakuliah Pemrograman Web. Silakan lakukan customisasi sesuai kebutuhan.</p>
+  <p>
+    Ujian ini bersifat open book. Anda diperbolehkan untuk mebuka <a class="btn btn-primary" href="https://laravel.com/docs/10.x" rel="noopener noreferrer" target="_blank">Dokumentasi</a> Laravel.</p>
+    
+  </p>
+  <p>Anda diperbolehkan untuk mencari referensi di internet, namun tidak diperbolehkan untuk berdiskusi dengan teman atau orang lain.</p>
+  <p>Semoga Sukses! <span class="text-danger">♥</span></p>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
